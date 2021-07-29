5d1ea1ba1c23bb710351523c0e78e11f674a5e3d
Artem Pryadkin <pryadkin1987@gmail.com>
2021-07-30 00:13:53 +0300

    update addPathToBase function, add tests

---
+++ tests/addPathToBase.spec.js
@@ -0,0 +1,85 @@
+const {MongoClient} = require('mongodb')
+const {addPathToBase} = require("../utils/addPathToBase")
+
+describe('addPathToBase: ', () => {
+	const testFilePath = 'природа/nature'
+	const existingFilePath = 'природа/корпоратив'
+	const originalPathsList = [
+		"bom",
+		"bom/Банско",
+		"bom/Оля",
+		"bom/озеро",
+		"nature",
+		"nature/вода",
+		"природа",
+		"природа/активный отдых",
+		"природа/активный отдых/эстония",
+		"природа/видео",
+		"природа/видео/уточки",
+		"природа/корпоратив"
+	]
+	let req = {
+		app: {locals: {configCollection: null}},
+		body: null
+	}
+	let emptyConfigCollection
+	let configCollection
+	let connection
+	let db
+	
+	beforeAll(async () => {
+		connection = await MongoClient.connect("mongodb://localhost:27017/", {
+			useUnifiedTopology: true,
+			useNewUrlParser: true
+		})
+		db = await connection.db('IDB')
+	})
+	
+	beforeEach(async () => {
+		configCollection = db.collection('testConfig')
+		req.app.locals.configCollection = configCollection
+		await configCollection.insertOne({name: "paths", pathsArr: originalPathsList})
+	})
+	
+	afterEach(async () => {
+		await configCollection.deleteMany({})
+	})
+	
+	afterAll(async () => {
+		// await emptyConfigCollection.deleteMany({})
+	})
+	
+	test('should create new config in DB if collection is not exists', async () => {
+		emptyConfigCollection = db.collection('testEmptyConfig')
+		req.app.locals.configCollection = emptyConfigCollection
+		
+		await addPathToBase(req, testFilePath)
+		const config = await emptyConfigCollection.findOne({name: "paths"})
+		expect(config.pathsArr).toHaveLength(1)
+		expect(config.pathsArr[0]).toBe(testFilePath)
+		
+		await req.app.locals.configCollection.drop()
+	})
+	test('should return an Error if collection is broken', async () => {
+		req.app.locals.configCollection = null
+		try {
+			await addPathToBase(req, testFilePath)
+		} catch (error) {
+			expect(error.message).toBe(`addPathToBase ERROR: insert path - ${testFilePath}, TypeError: Cannot read property 'findOne' of null`)
+		}
+	})
+	test('should return an empty string if new path exists in pathsArr', async () => {
+		const response = await addPathToBase(req, existingFilePath)
+		expect(response).toBe('')
+	})
+	test('should add new path to pathsArr', async () => {
+		await addPathToBase(req, testFilePath)
+		const { pathsArr } = await configCollection.findOne({name: "paths"})
+		expect(pathsArr).toHaveLength(13)
+		expect(pathsArr.includes(testFilePath)).toBeTruthy()
+	})
+	test('should return new filePath after adding it to pathsArr', async () => {
+		const response = await addPathToBase(req, testFilePath)
+		expect(response).toBe(testFilePath)
+	})
+})
--- utils/addPathToBase.js
+++ utils/addPathToBase.js
@@ -1,32 +1,33 @@
-const createError = require("http-errors")
-
+/**
+ * @param req
+ * @param {string} basePathWithoutRootDirectory
+ * @return {Promise<string>} basePathWithoutRootDirectory or '' if this directory is exists
+ */
 const addPathToBase = async (req, basePathWithoutRootDirectory) => {
 	const configCollection = req.app.locals.configCollection;
-	await configCollection.findOne({name: "paths"}, async function (err, res) {
-		if (err) {
-			console.log('configCollection.findOne (path) - oops!', err)
-			throw createError(400, `configCollection find path error`)
+	try {
+		const response = await configCollection.findOne({name: "paths"})
+		
+		if (!response) {
+			await configCollection.insertOne({name: "paths", pathsArr: [basePathWithoutRootDirectory]})
+			console.log(`ADD path "${basePathWithoutRootDirectory}" to config`)
+			return basePathWithoutRootDirectory
 		}
-		if (!res) {
-			await configCollection.insertOne({name: "paths", pathsArr: [basePathWithoutRootDirectory]}, function (err) {
-				if (err) {
-					console.log("Oops!- configCollection insert path error", err)
-					throw createError(400, `configCollection insert path error`)
-				}
-			})
-		} else {
-			const pathsSet = new Set(res.pathsArr)
-			pathsSet.add(basePathWithoutRootDirectory)
-			pathsSet.delete('')
-			const newPathsArr = Array.from(pathsSet).sort()
-			await configCollection.updateOne({name: "paths"}, {$set: {pathsArr: newPathsArr}}, function (err) {
-				if (err) {
-					console.log("Oops!- configCollection updateOne pathArr Error - ", err)
-					throw createError(400, `configCollection updateOne pathArr error`)
-				}
-			})
-		}
-	})
+		
+		const pathsSet = new Set(response.pathsArr)
+		if (pathsSet.has(basePathWithoutRootDirectory)) return ''
+
+		pathsSet.add(basePathWithoutRootDirectory)
+		pathsSet.delete('')
+		const newPathsArr = Array.from(pathsSet).sort()
+		
+		await configCollection.updateOne({name: "paths"}, {$set: {pathsArr: newPathsArr}})
+		console.log(`ADD PATH "${basePathWithoutRootDirectory}" to config`)
+		return basePathWithoutRootDirectory
+	} catch (error) {
+		console.log(`addPathToBase ERROR: insert path - ${basePathWithoutRootDirectory}`, error)
+		throw new Error(`addPathToBase ERROR: insert path - ${basePathWithoutRootDirectory}, ${error}`)
+	}
 }
 
 module.exports = {addPathToBase}
